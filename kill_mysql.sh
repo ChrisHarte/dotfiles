ba2a55ac58d45cfbf8a323358b8e7e1e74b71e2e
John Griffiths <john@johnantoni.com>
2012-08-10 17:51:55 -0400

    mysql

--- kill_mysql.sh
+++ kill_mysql.sh
@@ -8,5 +8,6 @@ rm -rf ~/Library/PreferencePanes/My*
 sudo rm -rf /Library/Receipts/mysql*
 sudo rm -rf /Library/Receipts/MySQL*
 sudo rm -rf /var/db/receipts/com.mysql.*
+sudo rm -rf /usr/local/var/mysql
 
 echo "sudo vim /etc/hostconfig # and remove the line MYSQLCOM=-YES-"
