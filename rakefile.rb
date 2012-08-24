1d65b94757a27aef850e0acbb1522af0114a721f
John Griffiths <john@johnantoni.com>
2012-08-24 13:13:26 -0400

    rake install

---
+++ rakefile.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/env ruby
+
+task :default do
+  # symlink configs
+  system("ln -s ~/.dotfiles/.gemrc ~/.gemrc")
+  system("ln -s ~/.dotfiles/.gitconfig ~/.gitconfig")
+  system("ln -s ~/.dotfiles/.githelpers ~/.githelpers")
+  system("ln -s ~/.dotfiles/.gitignore ~/.gitignore")
+  system("ln -s ~/.dotfiles/.powconfig ~/.powconfig")
+  system("ln -s ~/.dotfiles/.pryrc ~/.pryrc")
+  system("ln -s ~/.dotfiles/.zshrc ~/.zshrc")
+  system("ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf")
+
+  # for the mongo database
+  system("mkdir ~/.dotfiles/local_db")
+  system("ln -s ~/.dotfiles/.bundle_config ~/.bundle/config")
+end
+
+task :install do
+  # install vim from homebrew
+  system("brew install --HEAD https://raw.github.com/Homebrew/homebrew-dupes/master/vim.rb")
+
+  # test vim has ruby support
+  system("vim --version | grep ruby")
+
+  # install tmux
+  system("brew install tmux")
+end
+
