b8f3013ef7634f34600e76c06d7c95b4612ddff0
John Griffiths <john@johnantoni.com>
2013-01-28 11:09:08 -0500

    uninstalls

---
+++ uninstall/uninstall_xquartz.sh
@@ -0,0 +1,4 @@
+launchctl unload /Library/LaunchAgents/org.macosforge.xquartz.startx.plist
+sudo launchctl unload /Library/LaunchDaemons/org.macosforge.xquartz.privileged_startx.plist
+sudo rm -rf /opt/X11* /Library/Launch*/org.macosforge.xquartz.* /Applications/Utilities/XQuartz.app /etc/*paths.d/*XQuartz
+sudo pkgutil --forget org.macosforge.xquartz.pkg
