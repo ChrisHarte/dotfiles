1af7c7af2fb428d5f04b0b942a51e732e7e134b0
John Griffiths <john@johnantoni.com>
2013-05-26 10:13:49 -0500

    solr install instructions.

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -1,26 +1,12 @@
 alias reload="source ~/.zshrc"
 
-# COMPRESSION
-alias compress="tar -zcvf $0 $1"
 alias exe="chmod +x $*"
-
-# FILES
 alias concat="grep -r'<<<<' *"
 alias copy="cp -r $*" # recursive copy
 alias find="sudo find / -name $*"
 alias crush="optipng -o 7" 
-
-# KEYS
 alias hosts="sudo nano /etc/hosts" # edit hosts
-alias keygen="cd ~/.ssh && ssh-keygen -t rsa"
-alias verify="md5 $*" # check md5 tag for file
 
-# POWER
-alias stop="pkill $@" # kill by process name
-alias die="kill -9 $@" # stronger kill process
-alias poweroff="sudo shutdown -h -P +0" # power off machine after 0 minutes
-
-# SHORTCUTS
 alias free="free -m"
 alias la='ls -la'
 alias ls='ls -G'
@@ -37,6 +23,7 @@ alias vi=vim
 alias m=mvim
 alias ch="rm $HISTFILE"
 alias pcat='pygmentize'
+alias grep=ag
 
 # DEVELOPMENT
 alias git-pup="git pull && git submodule init && git submodule update && git submodule status"
@@ -52,7 +39,6 @@ alias migrate="bundle exec rake db:migrate"
 alias routes="rake routes"
 alias t="ruby -Ilib:test $*"
 alias worker="bundle exec rake jobs:work"
-alias kapow="touch tmp/restart.txt" # passenger/pow restart
 alias rake="bundle exec rake $*"
 alias log="tail -f log/development.log"
 alias chrome="/usr/bin/open -a '/Applications/Google Chrome.app'"
